2fa1dd60259d3ec2aea0fd5cfe66278c27599ec6
mehdi <mehdiyev.ayyhan@gmail.com>
2024-12-02 15:14:04 -0500

    hot fix 8

--- installers/macos/generate_installer.sh
+++ installers/macos/generate_installer.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 # Define variables
-APP_NAME="Example Installer"
-DMG_NAME="example-installer-1.0-mac.dmg"
-VOLUME_NAME="Example Installer"
-APP_FOLDER="example-installer-1.0-mac"
+APP_NAME="Example Installer.app" # Name of your application as it will appear in the DMG
+DMG_NAME="example-installer-1.0-mac.dmg" # Name of the output DMG file
+VOLUME_NAME="Example Installer" # Name of the DMG volume when mounted
+STAGING_DIR="staging" # Temporary directory for DMG content
 
-# Create a temporary directory to stage the files
-STAGING_DIR="staging"
-mkdir -p "$STAGING_DIR/$APP_FOLDER"
+# Create the staging directory
+mkdir -p "$STAGING_DIR/$APP_NAME"
 
-# Copy the executable and license into the staging directory
-cp ./app "$STAGING_DIR/$APP_FOLDER/"
-cp ../LICENSE.txt "$STAGING_DIR/$APP_FOLDER/"
+# Copy your application and license file into the staging directory
+cp ./app "$STAGING_DIR/$APP_NAME/" # Replace ./app with your actual app bundle or executable
+cp ../LICENSE.txt "$STAGING_DIR/$APP_NAME/"
 
-# Create the .dmg file
+# Create a symbolic link to the Applications folder in the staging directory
+ln -s /Applications "$STAGING_DIR/Applications"
+
+# Create the DMG file with drag-and-drop functionality
 hdiutil create \
   -volname "$VOLUME_NAME" \
   -srcfolder "$STAGING_DIR" \
